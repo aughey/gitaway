2a8c58297d66a7969dc87b5f973d9d6f8db9b92c
John Aughey <jha@aughey.com>
2010-11-14 09:04:52 -0600

    Cycling through all use for import testing.

--- import.rb
+++ import.rb
@@ -3,11 +3,13 @@ require './config/environment.rb'
 Repository.destroy_all
 system("rm -rf ./REPOS/*.git")
 
-u = User.find(:first)
+users = User.find(:all)
 
 for d in ARGV
   if File.exists?("#{d}/.git")
     puts "Importting git from #{d}"
+    u = users.pop
+    users.unshift u
     r = u.repositories.create(:name => File::basename(d))
     g = Grit::Repo.new(d)
     for b in g.branches
